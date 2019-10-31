4ea0745eb481d5e68a68d2292b617435b8c71437
bause_m1 <dfsdf>
2019-10-31 09:54:11 +0100

    - started work on updater

--- Controller/Settings/Update.py
+++ Controller/Settings/Update.py
@@ -10,7 +10,8 @@ class ReliatyUpdater:
 
     def check_for_update(self):
         repo = git.Repo(search_parent_directories=True)
-        yolo=""
+        local_version = repo.branches["master"].commit.hexsha
+        remote_version = repo.remotes.origin.refs["master"].commit.hexsha
 
 if __name__ == '__main__':
     r = ReliatyUpdater()
