a9300ca2fb9a3ea0dcaa0196ab9405d4e873ba3d
Max Bause <max.bause@live.de>
2020-06-20 20:33:41 +0200

    Additions to install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox \
-python3-pip python-setuptools python-wheel python3-pyqt5 python3-rpi.gpio git vlc -y
+python3-pip python-setuptools python-wheel python3-pyqt5 python3-rpi.gpio git vlc omxplayer -y
 cd /opt/ || exit 1
 sudo git clone https://github.com/buffb/ReliatyRadio.git
 cd ReliatyRadio/ || exit 1
@@ -14,7 +14,8 @@ sudo -H pip3 install -r requirements.txt
 
 #####Configuration
 echo 'Configuring the ReliatyRadio'
-sudo sed -i '$ a xset s off\nxset s noblank\nxset -dpms' /etc/xdg/openbox/autostart #disable display timeout aka screensaver
+sudo sed -i '$ a xset s off\nxset s noblank\nxset -dpms' /etc/xdg/openbox/autostart
+sudo sed -i '$ a omxplayer /opt/ReliatyRadio/Resources/intro.mp4' /etc/xdg/openbox/autostart #disable display timeout aka screensaver
 sudo sed -i '$ a sudo python3 /opt/ReliatyRadio/ReliatyRadio.py &' /etc/xdg/openbox/autostart # Radio autostart
 sudo sed -i -e '$i startx &' /etc/rc.local # Run startx in rc.local
 # Configure xinitrc
